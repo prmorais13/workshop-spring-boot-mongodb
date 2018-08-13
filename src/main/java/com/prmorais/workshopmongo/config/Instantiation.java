54e80b6c1b65cc7cccb6d4abbea23bb4827acd06
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-08-13 15:50:29 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/prmorais/workshopmongo/config/Instantiation.java
+++ src/main/java/com/prmorais/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.prmorais.workshopmongo.domain.Post;
 import com.prmorais.workshopmongo.domain.User;
 import com.prmorais.workshopmongo.dto.AuthorDTO;
+import com.prmorais.workshopmongo.dto.CommentDTO;
 import com.prmorais.workshopmongo.repository.PostRepository;
 import com.prmorais.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,14 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("04/05/2030"), "Partiu viagem", "Vou viajar para a Suíça. Abraços.", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("13/10/2018"), "Grato Senhor", "Incorporação publicada. Grato a todos.", new AuthorDTO(paulo));
 		Post post3 = new Post(null, sdf.parse("13/09/2018"), "Senhor", "Dai-me força para vencer meu próprio mal.", new AuthorDTO(paulo));
+		
+		CommentDTO comment1 = new CommentDTO("Que bom Nandinha. Deus de abençoe filha.", sdf.parse("04/10/2030"), new AuthorDTO(paulo));
+		CommentDTO comment2 = new CommentDTO("Graças a Deus que deu tudo certo pai. Beijos.", sdf.parse("13/10/2018"), new AuthorDTO(maria));
+		CommentDTO comment3 = new CommentDTO("Deus vai dar toda força que precisas pai. Te amo.", sdf.parse("13/09/2018"), new AuthorDTO(maria));
+		
+		post1.getComments().add(comment1);
+		post2.getComments().addAll(Arrays.asList(comment2));
+		post3.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepo.saveAll(Arrays.asList(post1, post2, post3));
 		
